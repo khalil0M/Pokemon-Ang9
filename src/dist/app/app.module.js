4984a929768aa6f68032c43402d586f4d6eff053
kkouis <kkouis@sqli.com>
2020-05-24 21:34:02 +0200

    les directives

--- src/dist/app/app.module.js
+++ src/dist/app/app.module.js
@@ -9,12 +9,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 const platform_browser_1 = require("@angular/platform-browser");
 const app_component_1 = require("./app.component");
+const border_card_directive_1 = require("./border-card.directive");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_1.AppComponent],
+        declarations: [app_component_1.AppComponent, border_card_directive_1.BorderCardDirective],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
